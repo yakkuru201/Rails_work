827f6d4a16e859861694d9b69f75302da865a52a
yakkuru201 <tonamimagic@gmail.com>
2013-12-14 15:13:44 +0900

    課題を答えた

--- demo/config/application.rb
+++ demo/config/application.rb
@@ -2,12 +2,17 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
+# Require the gems listed in Gemfile, including any gem
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
 module Demo
   class Application < Rails::Application
+
+    config.time_zone = 'Tokyo'
+ 
+    config.active_record.default_timezone = :local
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
